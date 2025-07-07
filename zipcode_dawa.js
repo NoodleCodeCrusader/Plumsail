7fa6f53b76cf4a5508ffb05100be33213fa483b6
NoodleCodeCrusader <skr@silkeborgforsyning.dk>
2025-07-07 12:44:15 +0200

    Update zipcode_dawa.js

--- zipcode_dawa.js
+++ zipcode_dawa.js
@@ -1,7 +1,4 @@
+// Gør funktionen global
 window.convertToUpper = function(cityName) {
   return cityName.toUpperCase();
-}
-
-// Test kald
-const result = convertToUpper("København");
-console.log(result); // Skriver "KØBENHAVN" i konsollen
+};
