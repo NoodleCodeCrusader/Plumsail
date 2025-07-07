9a223af9e4edb242816d7f553162a0b017dc9d39
NoodleCodeCrusader <skr@silkeborgforsyning.dk>
2025-07-07 14:44:45 +0200

    Update zipcode_dawa.js

--- zipcode_dawa.js
+++ zipcode_dawa.js
@@ -1,4 +1,32 @@
-// Gør funktionen global
-window.convertToUpper = function(cityName) {
-  return cityName.toUpperCase();
-};
+function DawaZipCodeLookUp () {
+    const zipcodefield = fd.field('post_nr');
+    const cityfield = fd.field('by');
+
+    zipcodefield.$on('change', function (value) {
+        const zipcode = value;
+
+        if (!zipcode || zipcode.length !== 4) {
+            cityfield.value = '';
+            return;
+        }
+
+        fetch('https://dawa.aws.dk/postnumre/' + postnummer)
+            .then(function (response) {
+                if (!response.ok) {
+                    throw new Error('Postnummer ikke fundet');
+                }
+                return response.json();
+            })
+            .then(function (data) {
+                if (data && data.navn) {
+                    cityfield.value = data.navn;
+                } else {
+                    cityfield.value = '';
+                }
+            })
+            .catch(function (error) {
+                console.error('Fejl ved opslag:', error);
+                cityfield.value = '';
+            });
+    });
+}
